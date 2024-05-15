82d53f3b72ea7a555093be727baad3998b539145
kiptootitus <kiptootitus75.com>
2024-05-15 15:05:35 +0300

    updates

--- chat/models.py
+++ chat/models.py
@@ -10,11 +10,31 @@ class User(AbstractUser):
     avatar = models.ImageField(null=True, default="avatar.svg")
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
     
   
-    permissions_related_name = 'custom_user_permissions'
-    groups_related_name = 'custom_user_groups'
+     # Custom related names for permissions and groups
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name=('user permissions'),
+        blank=True,
+        help_text=('Specific permissions for this user.'),
+        related_name='custom_user_permissions',
+    )
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name=('groups'),
+        blank=True,
+        help_text=('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        related_name='custom_user_groups',
+    )
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name =('user')
+        verbose_name_plural =('users')
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
